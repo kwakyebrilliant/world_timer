145703aeaf58822ed4f8073f7ad2ff35aae9e38b
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-08-25 12:20:29 +0000

    loading updates

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,1 +1,17 @@
 import 'package:flutter/material.dart';
+
+class loading extends StatefulWidget {
+  const loading({super.key});
+
+  @override
+  State<loading> createState() => _loadingState();
+}
+
+class _loadingState extends State<loading> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Text('loading'),
+    );
+  }
+}
