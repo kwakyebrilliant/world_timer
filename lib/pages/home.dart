f41c03493b0562cc124d13e711becb845754282c
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-08-25 15:38:17 +0000

    home updates

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -10,8 +10,18 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SafeArea(child: Text('home')),
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            TextButton.icon(
+              onPressed: () {},
+              icon: const Icon(Icons.edit_location),
+              label: const Text('Edit Location'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
