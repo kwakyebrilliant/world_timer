2f0d10ea9a7696f68811420e4e559c6044f8cce0
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-08-25 12:16:27 +0000

    location updates

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -1,1 +1,15 @@
 import 'package:flutter/material.dart';
+
+class ChooseLocation extends StatefulWidget {
+  const ChooseLocation({super.key});
+
+  @override
+  State<ChooseLocation> createState() => _ChooseLocationState();
+}
+
+class _ChooseLocationState extends State<ChooseLocation> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
