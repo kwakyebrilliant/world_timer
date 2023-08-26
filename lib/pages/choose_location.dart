b04d6de36d9729d6bc7cad2187b27f079ab5cc66
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-08-26 16:38:43 +0000

    location updates

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -8,26 +8,6 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
-  void getData() async {
-    //simulate a network request for a usename
-    String username = await Future.delayed(const Duration(seconds: 3), () {
-      return 'teleku';
-    });
-
-    //simulate a network request to print bio of a user
-    String bio = await Future.delayed(const Duration(seconds: 2), () {
-      return 'boys of the day, men at night';
-    });
-
-    print('$username - $bio');
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    getData();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
