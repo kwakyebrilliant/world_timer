58b57e6d02adff88e47c05c3b4d1791c83e5598d
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-08-26 16:40:47 +0000

    loading updates

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -8,19 +8,7 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
-  void getData() async {
-    //simulate a network request for a usename
-    String username = await Future.delayed(const Duration(seconds: 3), () {
-      return 'teleku';
-    });
-
-    //simulate a network request to print bio of a user
-    String bio = await Future.delayed(const Duration(seconds: 2), () {
-      return 'boys of the day, men at night';
-    });
-
-    print('$username - $bio');
-  }
+  void getData() async {}
 
   @override
   void initState() {
