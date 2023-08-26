86365ed13cd683d8ba24d4f75b3754601d9fd226
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-08-26 11:15:03 +0000

    counter added

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -8,14 +8,17 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+  int counter = 0;
+
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
+    print('initState function ran');
   }
 
   @override
   Widget build(BuildContext context) {
+    print('build function ran');
     return Scaffold(
       backgroundColor: Colors.green[200],
       appBar: AppBar(
@@ -23,7 +26,14 @@ class _ChooseLocationState extends State<ChooseLocation> {
         title: const Text('Choose your location'),
         centerTitle: true,
       ),
-      body: const Text('choose location'),
+      body: ElevatedButton(
+        onPressed: () {
+          setState(() {
+            counter += 1;
+          });
+        },
+        child: Text('counter is $counter'),
+      ),
     );
   }
 }
