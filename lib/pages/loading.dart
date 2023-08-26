e8d84516aa8f6b5f5ce269fe343b3dff3f6d7225
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-08-26 21:37:27 +0000

    updates

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
 
 class Loading extends StatefulWidget {
   const Loading({super.key});
@@ -10,25 +8,6 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
-  void getTime() async {
-    //make a request
-    final response = await http
-        .get(Uri.parse('http://worldtimeapi.org/api/timezone/Africa/Accra'));
-    Map data = jsonDecode(response.body);
-    //print(data);
-
-    //get properties from data
-    String datetime = data['datetime'];
-    String utc_offset = data['utc_offset'].substring(1, 3);
-    //print(datetime);
-    //print(utc_offset);
-
-    //create datetime object
-    DateTime now = DateTime.parse(datetime);
-    now = now.add(Duration(hours: int.parse(utc_offset)));
-    print(now);
-  }
-
   @override
   void initState() {
     super.initState();
--- lib/services/world_timer.dart
+++ lib/services/world_timer.dart
@@ -1,2 +1,30 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+
+class WoldTimer {
+  late String location; //location name for UI
+  late String time; //the time in that location
+  late String flag; //url to asset flag icon
+  late String url; //location url
+
+  void getTime() async {
+    //make a request
+    final response =
+        await http.get(Uri.parse('http://worldtimeapi.org/api/timezone/$url'));
+    Map data = jsonDecode(response.body);
+    //print(data);
+
+    //get properties from data
+    String datetime = data['datetime'];
+    String utc_offset = data['utc_offset'].substring(1, 3);
+    //print(datetime);
+    //print(utc_offset);
+
+    //create datetime object
+    DateTime now = DateTime.parse(datetime);
+    now = now.add(Duration(hours: int.parse(utc_offset)));
+
+    //set the time property
+    time = now.toString();
+  }
+}
