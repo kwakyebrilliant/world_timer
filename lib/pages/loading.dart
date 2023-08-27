80603a1102536d4582acc44f6cdc0d57c2463f7a
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-08-27 00:21:58 +0000

    loading updates

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -15,10 +15,6 @@ class _LoadingState extends State<Loading> {
     WorldTimer instance = WorldTimer(
         location: 'Berlin', flag: 'germany.png', url: 'Europe/Berlin');
     await instance.getTime();
-    print(instance.time);
-    setState(() {
-      time = instance.time;
-    });
   }
 
   @override
