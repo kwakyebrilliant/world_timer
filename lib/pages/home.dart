d9b59f66e872de3191573c1b2c29f8e41b10bbd1
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-08-28 14:12:13 +0000

    home updates

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -22,45 +22,54 @@ class _HomeState extends State<Home> {
     return Scaffold(
       body: SafeArea(
         child: Center(
-          child: Padding(
-            padding: const EdgeInsets.fromLTRB(0.0, 300.0, 0.0, 0.0),
-            child: Column(
-              children: <Widget>[
-                TextButton.icon(
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/location');
-                  },
-                  icon: Icon(
-                    Icons.edit_location,
-                    color: Colors.green[500],
-                  ),
-                  label: Text(
-                    'Edit Location',
-                    style: TextStyle(color: Colors.green[500]),
-                  ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text(
-                      data['location'],
-                      style: const TextStyle(
-                        fontSize: 28.0,
-                        letterSpacing: 2.0,
-                      ),
+          child: Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/$bgImg'),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(0.0, 300.0, 0.0, 0.0),
+              child: Column(
+                children: <Widget>[
+                  TextButton.icon(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/location');
+                    },
+                    icon: Icon(
+                      Icons.edit_location,
+                      color: Colors.grey[100],
                     ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 10.0,
-                ),
-                Text(
-                  data['time'],
-                  style: const TextStyle(
-                    fontSize: 66.0,
+                    label: Text(
+                      'Edit Location',
+                      style: TextStyle(color: Colors.grey[100]),
+                    ),
                   ),
-                ),
-              ],
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        data['location'],
+                        style: const TextStyle(
+                          fontSize: 28.0,
+                          letterSpacing: 2.0,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 10.0,
+                  ),
+                  Text(
+                    data['time'],
+                    style: TextStyle(
+                      fontSize: 66.0,
+                      color: Colors.grey[100],
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
